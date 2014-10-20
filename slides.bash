e7bfdd81d9c264347156074ed58c63092ceb5f09
Daniel Cargill <daniel@danielcargill.com>
2014-10-20 07:14:45 -0600

    initial commit

---
+++ slides.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+#####################
+
+#####################
+
+
+
+if [ -z "$1" ]
+then
+  echo "No input file"
+  echo "Help: $0 -h"
+  exit
+else
+    case $1 in
+#         $IPADDRESS)
+#            echo "That's your ip stupid! You can't mount that."
+#            exit;;
+        -h) echo "ipython nbconvert INPUTFILE.ipynb --to slides --post serve"
+#            echo "ex: 192.168.2.2"
+            exit;;
+         *)
+            INPUTFILE=$1
+    esac
+    
+fi
+ipython nbconvert $INPUTFILE.ipynb --to slides --reveal-prefix "http://cdn.jsdelivr.net/reveal.js/2.5.0"
+
+
